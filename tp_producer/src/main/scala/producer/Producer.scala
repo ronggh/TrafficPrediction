50a92ebb2153dca44c55af44a032528983a0bd22
ronggh@sina.cn <ronggh@sina.cn>
2019-01-26 22:02:10 +0800

    commit finish

--- tp_producer/src/main/scala/producer/Producer.scala
+++ tp_producer/src/main/scala/producer/Producer.scala
@@ -28,8 +28,14 @@ object Producer {
 
     val df = new DecimalFormat("0000")
 
-    // 产生模拟数据
-    while (true) {
+    // 产生模拟数据，生产1个小时的数据
+    // 开始计时时间
+    val beginTime = Calendar.getInstance().getTimeInMillis()
+    // 持续1小时，1 * 60 分钟 * 60 秒 * 1000 毫秒
+    val duration = 1 *  60 * 60 * 1000
+    // 结束计时时间
+    var endTime =  Calendar.getInstance().getTimeInMillis()
+    while (endTime - beginTime <= duration) {
       // 模拟产生监测点，1-20
       val monitorId = df.format(Random.nextInt(20) + 1)
       // 模拟车速
@@ -61,6 +67,9 @@ object Producer {
 
       // 每200毫秒产生一条模拟数据
       Thread.sleep(200)
+
+      // 重新计算结束时间：以判断是否到了1小时
+      endTime =  Calendar.getInstance().getTimeInMillis()
     }
   }
 }
